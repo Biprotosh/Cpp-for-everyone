04d554941952d91375b350591cca702b36417417
Osanda Lakshitha <76400536+OsandaLakshitha@users.noreply.github.com>
2023-03-05 14:57:12 +0530

    Number Guessing game in C++
    Simple number guessing game i made by C++

---
+++ NumberGuessing.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<ctime>
+
+int main()
+{
+    srand(time(NULL));
+
+    int num = rand() % 100 +1;
+
+    int guess;
+    int tries;
+
+    std::cout << "*************  NUMBER GUESSING GAME  *****************\n";
+
+    do{
+        std::cout << "Guess a random Number between 1-100 : ";
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num)
+        {
+            std::cout << "Number is Lower\n";
+        }
+
+        else if(guess < num)
+        {
+            std::cout << "Number is Higher\n";
+        }
+
+        else
+        {
+            std::cout << "##### You guessed Correct #####" << "  You tried for  "<< tries << " Times \n";
+        }
+
+    }while(guess != num);
+
+
+    std::cout << "\n\n********************************************************\n";
+}
